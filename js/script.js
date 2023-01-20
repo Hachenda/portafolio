85940c309a3bb34073e561389db3a8611625d86e
David Hernández <106157729+Hachenda@users.noreply.github.com>
2023-01-20 15:49:04 +0100

    Update

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@
 let botonVolver = document.getElementById("volverArriba");
 let estrellas = document.getElementById("estrellas");
 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -13,10 +12,9 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function volverArriba() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
 
 function numRandom(min, max) {
@@ -30,7 +28,6 @@ function addEstrellas(){
     stringEstrellas += ",";
    }
    estrellas.style.boxShadow = (stringEstrellas.substring(0, stringEstrellas.length - 1)).valueOf();
-  console.log(stringEstrellas.substring(0, stringEstrellas.length - 1)) 
 }
 addEstrellas();
 
